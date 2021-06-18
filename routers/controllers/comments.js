ce11e7cb779ac284c2a346fdcf9ea38462e356fa
M_abedalnabi <abedalnabi96@gmail.com>
2021-06-18 22:32:00 +0300

    solve all

--- routers/controllers/comments.js
+++ routers/controllers/comments.js
@@ -1,21 +1,18 @@
 const commentsModel = require("./../../db/models/comments");
 
 const createNewComment = (req, res) => {
-  const { comment, commenter } = req.body;
+  const query = `INSERT INTO comments (comment ,article_id  ) VALUES (? , ? )`;
 
-  const newComment = new commentsModel({
-    comment,
-    commenter,
+  const data = [req.body.comment, req.params.id];
+  connection.query(query, data, (err, results) => {
+    console.log(results);
   });
-
-  newComment
-    .save()
-    .then((result) => {
-      res.status(201).json(result);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
+  const query1 = `select * from comments where comment = ?`;
+  const data1 = [req.body.comment];
+  connection.query(query1, data1, (err, results) => {
+    console.log(results);
+    res.json(results);
+  });
 };
 
 module.exports = {
